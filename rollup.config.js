4f71b9faa2c5aeb03dbcbfc3bbb78c84d4996545
SouzaGabrielC <gabriel.souza@tarmac.io>
2024-08-30 09:57:16 -0300

    Improve type exporting to be more compatible with new versions of typescript.

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,13 @@ import autoExternal from 'rollup-plugin-auto-external';
 import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
+import { dts } from 'rollup-plugin-dts';
 
-import pkg from './package.json' with { type: "json" };
+import pkg from './package.json' assert { type: "json" };
 
 const extensions = ['.ts'];
 
-export default {
+export default [{
     input: 'src/index.ts',
     output: [
         {
@@ -35,4 +36,11 @@ export default {
             extensions,
         })
     ]
-};
+}, {
+    input: "./dist/types/index.d.ts",
+    output: [
+        { file: "dist/es/easy-template-x.d.ts", format: "es" },
+        { file: "dist/cjs/easy-template-x.d.ts", format: "cjs" }
+    ],
+    plugins: [dts()],
+},];
